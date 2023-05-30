493b4f69754e4c7a08063343998398a60ad43599
Ondrej Sika <ondrej@ondrejsika.com>
2023-05-30 13:15:41 +0200

    docs(pkg/hello): Add basics docs

--- pkg/hello/hello.go
+++ pkg/hello/hello.go
@@ -1,11 +1,14 @@
+// Package hello provides a simple greeting.
 package hello
 
 import "fmt"
 
+// SayHello returns a string containing a greeting.
 func SayHello(name string) string {
 	return fmt.Sprintf("Hello %s!\n", name)
 }
 
+// PrintSayHello prints a greeting.
 func PrintSayHello(name string) {
 	fmt.Print(SayHello(name))
 }
