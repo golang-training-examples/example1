72d48d248233ee5e1ee9cad8b0c86eacc1fe49b9
Ondrej Sika <ondrej@ondrejsika.com>
2023-05-30 13:31:09 +0200

    docs: Add docs to main pkg

--- main.go
+++ main.go
@@ -1,3 +1,15 @@
+/*
+Example1 is a simple app for Golang Training
+
+Usage:
+
+	example1 hello
+
+For more information, see:
+
+	example1 --help
+*/
+
 package main
 
 import "github.com/golang-training-examples/example1/cmd"
